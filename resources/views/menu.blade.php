24de629e780c49143e437246d6fab895f98703f3
Abaev <abaev@image-spark.ru>
2021-10-05 12:24:53 +0400

    First commit

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+
+        <title>Menu</title>
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+            <p> Menu </p>
+    </body>
+</html>
